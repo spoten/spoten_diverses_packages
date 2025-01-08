e822a710efec46565faf004d315d697f9d71440d
thaysg <thay.a.garcia@gmail.com>
2025-01-08 11:12:07 -0300

    change name qrcode folder

--- lib/qr_code_scanner/src/web/flutter_qr_stub.dart
+++ lib/qr_code_scanner/src/web/flutter_qr_stub.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:spoten_packages/qrcode/src/types/camera.dart';
+import 'package:spoten_packages/qr_code_scanner/src/types/camera.dart';
 
 Widget createWebQrView(
         {onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
